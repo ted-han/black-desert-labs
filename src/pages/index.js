578da053f71ef19296c175f56e66c2aed289a413
ted-han <taekyung28h@gmail.com>
2021-11-19 12:15:25 +0900

    fix: index페이지
    - 불필요한 부분 삭제

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,98 +1,15 @@
-import React, { useState } from "react";
-import { graphql } from "gatsby";
+import React from "react";
+import SearchComponent from "../components/search";
 
-import Search from "../components/search";
-
-const dd = [
-  { id: "edde8055-84f6-5637-a8d9-36ce4c443b66", item_name: "옥수수" },
-  { id: "e7c5ed32-1932-5e61-bf7c-67b283419f5a", item_name: "사과" },
-  { id: "a7f77534-a33f-5ae9-9110-81b9bd354b69", item_name: "양배추" },
-  { id: "fa70b79e-2718-50bf-a515-b6325e3c39df", item_name: "설탕" },
-  { id: "59bc5a17-1456-5511-9424-23a832d144d3", item_name: "발효제" },
-  { id: "6972f414-59f1-5d47-b5e0-4409ab41d80d", item_name: "식초" },
-  { id: "91ca53a8-e9e4-5d3f-a6af-363b841ab340", item_name: "채소 절임" },
-];
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-
-const IndexPage = ({ data }) => {
-  const [updateData, setUpdateData] = useState([]);
-  const [value, setValue] = useState("");
-  console.log(data);
-  console.log(JSON.stringify(data.allCraft.nodes));
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(value);
-  };
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    setValue(event.target.value);
-    setUpdateData(
-      dd.filter(
-        (v) =>
-          v.item_name.includes(event.target.value) && event.target.value !== "",
-      ),
-    );
-  };
-  console.log(updateData);
-
+const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>검은사막</title>
-      <h1>검은사막</h1>
-      <Search />
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input type="text" value={value} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      <div>
-        {dd
-          .filter((v) => v.item_name.includes(value) && value !== "")
-          .map((v) => (
-            <div key={v.id}>
-              <a href={v.item_name.replace(/ /gi, "-")}>{v.item_name}</a>
-            </div>
-          ))}
-      </div>
-      ㅏㅏㅏ
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div>
-        {data.allCraft.nodes.map((v) => (
-          <div key={v.id}>
-            <a href={v.item_name.replace(/ /gi, "-")}>{v.item_name}</a>
-          </div>
-        ))}
-      </div>
+      <h1>검은사막 연구소</h1>
+      <SearchComponent />
+      <h2>검은사막 연구소</h2>
     </main>
   );
 };
-
-export const query = graphql`
-  {
-    allCraft {
-      nodes {
-        id
-        item_name
-      }
-    }
-  }
-`;
 
 export default IndexPage;
