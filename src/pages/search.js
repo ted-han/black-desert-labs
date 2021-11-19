dd3fd31ca3f2705190ebd4dfbc08a4ca6d20ef88
ted-han <taekyung28h@gmail.com>
2021-11-19 12:11:14 +0900

    add: 검색결과 페이지 추가
    - url 쿼리에 따라 해당하는 검색결과 처리

---
+++ src/pages/search.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { graphql } from "gatsby";
+
+import SearchComponent from "../components/search";
+import SearchResultComponent from "../components/search-result";
+
+const IndexPage = ({ data }) => {
+  const [query, setQuery] = useState("");
+  const filterData = data.allCraft.nodes.filter(
+    (v) => v.item_name.includes(query) && query !== "",
+  );
+
+  useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const param = urlSearchParams.get("q");
+    setQuery(param);
+    return;
+  }, [window.location.search]);
+
+  return (
+    <main>
+      <title>검은사막</title>
+      <SearchComponent page="search" />
+      <SearchResultComponent query={query} data={filterData} />
+    </main>
+  );
+};
+
+export const query = graphql`
+  {
+    allCraft {
+      nodes {
+        id
+        item_id
+        item_name
+      }
+    }
+  }
+`;
+
+export default IndexPage;
