1c4a4cc3b47be82ed3605bdf2c729665a927e3b9
ted-han <taekyung28h@gmail.com>
2021-11-19 23:11:18 +0900

    add: 아이템 상세 페이지
    - 상단: 아이템 제작 정보
    - 하단: 해당아이템으로 만들수 있는 아이템목록

--- src/components/item-info.js
+++ src/components/item-info.js
@@ -1,24 +1,35 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
 
-const HowIngredient = ({ allIngredient }) => {
-  console.log(allIngredient);
-  return (
-    <div>
-      {allIngredient.nodes.map((v) => {
-        return (
-          <div key={v.name}>
-            {v.name} = {v.craft.map((c) => c.item_name)}
-          </div>
-        );
-      })}
+import {
+  ingredientWrapper,
+  itemBox,
+  itemBoxHeader,
+  itemWrapper,
+  itemWrapperHeader,
+  itemWrapperFooter,
+} from "./item.module.css";
+
+const ItemBox = ({ data }) => (
+  <div
+    className={itemBox}
+    onClick={() => {
+      navigate(`/${data.item_name.replace(/ /gi, "-")}`);
+    }}
+  >
+    <div className={itemBoxHeader}>
+      <img alt={`${data.item_name}`} src={`/${data.item_id}.png`} width="30" />
+      <span>x{data.cnt}</span>
     </div>
-  );
-};
+    <span>{data.item_name}</span>
+  </div>
+);
 
 const ItemInfo = ({ data }) => {
   if (!data) return null;
   console.log(data);
+
+  // 현재 아이템의 재료 중 조합이 필요한 아이템이 있는지 확인
   const arr = [];
   const getAllCraftWay = (o, cnt) => {
     o.craft.map((v, index) => {
@@ -33,78 +44,158 @@ const ItemInfo = ({ data }) => {
         ...v,
         total: cnt * v.cnt,
       });
-      return getAllCraftWay(v, cnt * v.cnt);
+      return getAllCraftWay({ ...v, craft: v.craft }, cnt * v.cnt);
     });
   };
 
   getAllCraftWay(data.craft, data.craft.cnt);
-
   console.log(data.craft.craft);
   console.log(arr);
+
+  // 페이지 상단
+  // 아이템 설명, 제조, 하위제조, 필요한 모든 기본아이템
   const HowCraft = ({ craft, arr }) => {
-    console.log(arr);
-    // 가장 기본이 되는 원재료인 경우
+    // 가장 기본이 되는 원재료인 경우, 조합이 필요없는 경우
     if (craft.is_basic) {
       return (
-        <div>
-          <div>{data.craft.item_name}</div>
+        <div className={itemWrapperHeader}>
+          <img
+            alt={`${craft.item_name}`}
+            src={`/${craft.item_id}.png`}
+            width="60"
+            height="60"
+          />
+          <h4>{craft.item_name}</h4>
         </div>
       );
     }
-    return (
-      <div>
-        {craft.craft.map((v) => {
-          return (
-            <div key={v.item_id}>
-              {v.item_name} {v.cnt}
-            </div>
-          );
-        })}
-        <br />
-        ------------
-        {craft.craft.length !== arr.length && (
-          <div>
-            {arr
-              .filter((v) => !v.is_basic)
-              .map((vv) => {
-                return (
-                  <div>
-                    {vv.item_name}
-                    {vv.craft.map((c) => c.item_name)}
-                  </div>
-                );
-              })}
 
-            {Object.keys(
-              arr.reduce((acc, curr) => {
-                if (!curr.is_basic) return acc;
-                if (acc[curr.item_name]) {
-                  acc[curr.item_name].cnt = acc[curr.item_name].cnt + curr.cnt;
+    return (
+      <>
+        <div className={itemWrapperHeader}>
+          <img
+            alt={`${craft.item_id}`}
+            src={`/${craft.item_id}.png`}
+            width="60"
+            height="60"
+          />
+          <h4>{craft.item_name}</h4>
+        </div>
+        <h3>조합</h3>
+        <div id={ingredientWrapper}>
+          <table>
+            <thead>
+              <tr>
+                <th>아이템</th>
+                <th>재료</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <Link to={`/${craft.item_name.replace(/ /gi, "-")}`}>
+                    {craft.item_name}
+                  </Link>
+                </td>
+                <td>
+                  <div className={itemWrapper}>
+                    {craft.craft.map((c) => {
+                      return <ItemBox data={c} />;
+                    })}
+                  </div>
+                </td>
+              </tr>
+              {arr
+                .filter((v) => !v.is_basic)
+                .map((vv) => {
+                  return (
+                    <tr>
+                      <td>
+                        <Link to={`/${vv.item_name.replace(/ /gi, "-")}`}>
+                          {vv.item_name}
+                        </Link>
+                      </td>
+                      <td>
+                        <div className={itemWrapper}>
+                          {vv.craft.map((c) => {
+                            return <ItemBox data={c} />;
+                          })}
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+
+        <div className={itemWrapperFooter}>
+          {craft.craft.length !== arr.length && (
+            <div className={itemWrapper}>
+              {Object.values(
+                arr.reduce((acc, curr) => {
+                  if (!curr.is_basic) return acc;
+                  if (acc[curr.item_name]) {
+                    acc[curr.item_name].cnt =
+                      acc[curr.item_name].cnt + curr.cnt;
+                    return acc;
+                  }
+                  acc[curr.item_name] = curr;
                   return acc;
-                }
-                acc[curr.item_name] = curr;
-                return acc;
-              }, {}),
-            ).map((v) => v.item_name)}
-          </div>
-        )}
-      </div>
+                }, {}),
+              ).map((v) => {
+                return <ItemBox data={v} />;
+              })}
+            </div>
+          )}
+        </div>
+      </>
     );
   };
 
-  // // 0. 아래꺼가 true면 가장 낮은 레벨인거임, 획득방법 표시
-  // console.log(craft.is_basic);
-  // // 1. 아래꺼가 맨 첫줄, 메인조합
-  // console.log(craft.craft);
-  // // 2. 아래껀 첫줄중에 조합필요한거 보여주면 되는데 is_basic false인거만 약간 차이둬서 보여주면됨.
-  // console.log(arr);
-  // // 3. 아래 중 is_basic true 인건 total로 해서 젤 아래에 보여주기, 조건 craft.craft.length !== arr.length
-  // console.log(arr);
+  // 페이지 하단
+  // 해당 아이템을 이용하여 만들 수 있는 아이템 전체
+  const HowIngredient = ({ allIngredient }) => {
+    if (allIngredient.nodes.length === 0) return null;
 
-  // if (craft.is_basic) {
-  //   return <div>{craft.craft.map((v) => v.item_name)}</div>;
-  // }
+    return (
+      <>
+        <h3>상위 아이템</h3>
+        <div id={ingredientWrapper}>
+          <table>
+            <thead>
+              <tr>
+                <th>아이템</th>
+                <th>재료</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allIngredient.nodes.map((v) => {
+                return (
+                  <tr>
+                    <td>
+                      <Link to={`/${v.name.replace(/ /gi, "-")}`}>
+                        {v.name}
+                      </Link>
+                    </td>
+                    <td>
+                      <div className={itemWrapper}>
+                        {v.craft.map((c) => {
+                          return <ItemBox data={c} />;
+                        })}
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  };
 
+  console.log(1);
   return (
     <div>
       <HowCraft craft={data.craft} arr={arr} />
